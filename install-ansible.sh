084484cd9bb7d73b2144c9c3cbd0c2a0b144271f
Christopher Gervais <chris@ergonlogic.com>
2015-02-07 20:32:26 -0500

    Initial commit.

---
+++ install-ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$(id -u)" != "0" ]; then
+  echo "Sorry, this script must be run as root."
+  exit 1
+fi
+
+command -v ansible >/dev/null 2>&1
+if [ $? -eq 1 ]; then
+
+  aptitude update
+  aptitude -y install git python-setuptools
+
+  ansible_dir=/usr/local/lib/ansible/
+  if [ ! -d $ansible_dir ]; then
+    git clone --depth=1 git://github.com/ansible/ansible.git $ansible_dir
+  fi
+
+  cd $ansible_dir
+  python ./setup.py install
+
+fi
