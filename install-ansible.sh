265af0968139fa2b92404f3dce78656d1f63473e
Christopher Gervais <chris@ergonlogic.com>
2015-02-09 17:57:54 -0500

    Make install quieter.

--- install-ansible.sh
+++ install-ansible.sh
@@ -5,18 +5,21 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-command -v ansible >/dev/null 2>&1
+which ansible >/dev/null 2>&1
 if [ $? -eq 1 ]; then
 
-  aptitude update
-  aptitude -y install git python-setuptools
+  echo "Installing Ansible build dependencies."
+  apt-get -qq --force-yes update > /dev/null 2>&1
+  apt-get -qq --force-yes install git python-setuptools >/dev/null 2>&1
 
   ansible_dir=/usr/local/lib/ansible/
   if [ ! -d $ansible_dir ]; then
-    git clone --recursive --depth=1 git://github.com/ansible/ansible.git $ansible_dir
+    echo "Cloning Ansible."
+    git clone --quiet --recursive --depth=1 git://github.com/ansible/ansible.git $ansible_dir >/dev/null 2>&1
   fi
 
+  echo "Running setups tasks for Ansible."
   cd $ansible_dir
-  python ./setup.py install
+  python ./setup.py install >/dev/null 2>&1
 
 fi
